61e7b7ebf3817f950cb7e40218b69df00e63d094
Raynos <raynos2@gmail.com>
2013-01-26 21:39:39 -0800

    make tests more explicit

--- test/index.js
+++ test/index.js
@@ -14,15 +14,50 @@ test("console has expected methods", function (assert) {
     assert.end()
 })
 
-test("all the methods", function (assert) {
+test("invoke console.log", function (assert) {
     console.log("test-log")
+
+    assert.end()
+})
+
+test("invoke console.info", function (assert) {
     console.info("test-info")
+
+    assert.end()
+})
+
+test("invoke console.warn", function (assert) {
     console.warn("test-warn")
-    console.dir("test-dir")
+    
+    assert.end()
+})
+
+test("invoke console.time", function (assert) {
     console.time("label")
-    console.timeEnd("label")
+
+    assert.end()
+})
+
+test("invoke console.trace", function (assert) {
     console.trace("test-trace")
+
+    assert.end()
+})
+
+test("invoke console.assert", function (assert) {
     console.assert(true)
+
+    assert.end()
+})
+
+test("invoke console.dir", function (assert) {
+    console.dir("test-dir")
+
+    assert.end()
+})
+
+test("invoke console.timeEnd", function (assert) {
+    console.timeEnd("label")
 
     assert.end()
 })
